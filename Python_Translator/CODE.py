22e9fcdaf2383693ee630107e938fe461ad5f0da
Satya Sharma <71933842+SatyaSharma73@users.noreply.github.com>
2020-11-13 19:15:15 +0530

    User Decides the language
    2nd Commit

--- Python_Translator/CODE.py
+++ Python_Translator/CODE.py
@@ -2,14 +2,29 @@ from googletrans import *
 from gtts import gTTS
 import os
 
-text = '''The computer was born to solve problems that did not exist before'''
-       
+#text = '''The computer was born to solve problems that did not exist before'''
+print("Langauges : ")
+print('''
+            af'--'Afrikaans', 'sq'--'Albanian','ar'--'Arabic','hy'--'Armenian',
+            'bn'--'Bengali','ca'--'Catalan','zh'--'Chinese','zh-cn'--'Chinese (Mandarin/China)',
+            'zh-tw'--'Chinese (Mandarin/Taiwan)','zh-yue'--'Chinese (Cantonese)','hr'--'Croatian',
+            'cs'--'Czech','da'--'Danish','nl'--'Dutch','en'--'English','en-au'--'English (Australia)',
+            'en-uk'--'English (United Kingdom)','en-us'--'English (United States)','fi'--'Finnish',
+            'fr'--'French','de'--'German','el'--'Greek','hi'--'Hindi','hu'--'Hungarian','is'--'Icelandic',
+            'id'--'Indonesian','it'--'Italian','ja'--'Japanese','ko'--'Korean','la'--'Latin','lv'--'Latvian',
+            'mk'--'Macedonian','no'--'Norwegian','pl'--'Polish','pt'--'Portuguese',
+            'pt-br'--'Portuguese (Brazil)','ro'--'Romanian','ru'--'Russian','sr'--'Serbian',
+            'sk'--'Slovak','es'--'Spanish','es-es'--'Spanish (Spain)','es-us'--'Spanish (United States)',
+            'sw'--'Swahili','sv'--'Swedish','ta'--'Tamil','th'--'Thai',
+            'tr'--'Turkish','vi'--'Vietnamese','cy'--'Welsh''')
+lang=input("\nEnter the langauge You want to convert into ")
+text=input("Enter Word/phrase")
 translator = Translator()            
 temp= translator.detect(text)
 print('-----------------------------------------------------------------------')
 print('The Text is :')
 print(text,"\n")
-translated = translator.translate(text,dest='hi') #Change en into any langauge you want from below list
+translated = translator.translate(text,dest=lang) #Change en into any langauge you want from below list
 
 # 'af':'Afrikaans', 'sq':'Albanian','ar':'Arabic','hy':'Armenian','bn':'Bengali','ca':'Catalan','zh':'Chinese','zh-cn':'Chinese (Mandarin/China)',
 # 'zh-tw':'Chinese (Mandarin/Taiwan)','zh-yue':'Chinese (Cantonese)','hr':'Croatian','cs':'Czech','da':'Danish','nl':'Dutch','en':'English','en-au':'English (Australia)',
